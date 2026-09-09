76620ce2aea6aecd6ff00755c5732ecf488a7f4f
Syzdyq-b <bmxq6sdcjn@privaterelay.appleid.com>
2026-09-09 21:25:54 +0500

    Add director and client

---
+++ src/ComputerDirector.java
@@ -0,0 +1,20 @@
+public class ComputerDirector {
+
+    public Computer buildGamingcomputer(){
+        return new ComputerBuilder()
+                .setCpu("intel i7")
+                .setRam(32)
+                .setStorage(1000)
+                .setGpu("RTX 4070")
+                .build();
+
+    }
+    public Computer buildofficcomputer(){
+        return new ComputerBuilder()
+                .setCpu("intel i5")
+                .setRam(16)
+                .setStorage(512)
+                .setGpu("integrated graphics")
+                .build();
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,30 +1,23 @@
 public class Main {
+
     public static void main(String[] args) {
 
-        JobListing job1 = new JobListing("Java Developer", 300000, "Backend development");
-        JobListing job2 = new JobListing("Web Designer", 200000, "Figma + UI/UX");
+        Computer customComputer = new ComputerBuilder()
+                .setCpu("AMD Ryzen 7")
+                .setRam(16)
+                .setStorage(512)
+                .setGpu("RTX 4060")
+                .build();
 
-        Freelancer f1 = new Freelancer("Beknur", "Java", 4.9);
-        Freelancer f2 = new Freelancer("Aknur", "UI/UX", 4.7);
+        System.out.println("Custom computer:");
+        System.out.println(customComputer);
 
-        Portal portal = new Portal("Freelance.kz");
 
-        portal.addJob(job1);
-        portal.addJob(job2);
+        ComputerDirector director = new ComputerDirector();
 
-        portal.addFreelancer(f1);
-        portal.addFreelancer(f2);
+        Computer gamingComputer = director.buildGamingcomputer();
 
-        portal.printPortalInfo();
-
-        job1.printInfo();
-        job2.printInfo();
-
-        f1.printInfo();
-        f2.printInfo();
-
-        System.out.println("Comparison:");
-        System.out.println("Is Java Developer job payment higher? " + (job1.getPayment() > job2.getPayment()));
-        System.out.println("Does Beknur have a higher rating? " + (f1.getRating() > f2.getRating()));
+        System.out.println("\nGaming computer:");
+        System.out.println(gamingComputer);
     }
 }
